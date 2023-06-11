d900d333467ae0c624d97819a44885b0822456a1
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-12 01:10:07 +0300

    upgrede dz №1

--- siminar1/ConsoleApp1/Program.cs
+++ siminar1/ConsoleApp1/Program.cs
@@ -9,7 +9,14 @@ int number = Convert.ToInt16(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number_2 = Convert.ToInt16(Console.ReadLine());
 if (number > number_2)
+{
     Console.WriteLine($"максимальное число : {number}, а минимальное: {number_2}");
+}
 else
+{
     Console.WriteLine($"максимальное число : {number_2}, а минимальное: {number}");
-
+}
+if (number == number_2)
+{
+    Console.WriteLine("Числа равны");
+}
