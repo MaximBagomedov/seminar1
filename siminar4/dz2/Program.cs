8a689b7b393958cfab061d16fc5883fa8ecfa327
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-22 22:53:29 +0300

    Dz2_4seminar

---
+++ siminar4/dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Number(int number)
+{
+    
+    int counter = Convert.ToString(number).Length;
+    int ostatok = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      ostatok = number - number % 10;
+      result = result + (number - ostatok);
+      number = number / 10;
+    }
+   return result;
+}
+int summa = Number(number);
+Console.WriteLine(summa);  
