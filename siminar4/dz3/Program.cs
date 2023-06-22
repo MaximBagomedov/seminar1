91308792439e64fbb3a599a8fa05388c486919f3
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-22 23:32:24 +0300

    dz3_4seminar

---
+++ siminar4/dz3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// одномерные с Рандомом
+/*int []array = new int [8];
+Random random = new Random ();
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = random.Next (0, 255);
+    Console.Write($"{array[i]}, ");
+}*/
+int []array = {1, 2, 5, 7, 19, 20, int.MaxValue, int.MinValue};
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.Write("]");
